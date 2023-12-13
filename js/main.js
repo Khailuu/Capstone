ed84063bb54fd7b1e4ad27f713a185bd0936aa74
khailuu <khailuu123@gmail.com>
2023-12-14 00:03:55 +0700

    update

--- js/main.js
+++ js/main.js
@@ -41,6 +41,8 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+
+
 // $(document).ready(function(){
 //   $("#navbarNavDropdown").hide();
 //   $(".trigger").click(function(){
@@ -56,4 +58,131 @@ const bar = document.querySelector('.navbar-toggler');
 //   })
 // }
 
+
+/**
+ * File navigation.js.
+ *
+ * Handles toggling the navigation menu for small screens and enables TAB key
+ * navigation support for dropdown menus.
+ */
+( function() {
+	const siteNavigation = document.getElementById( 'site-navigation' );
+
+	// Return early if the navigation don't exist.
+	if ( ! siteNavigation ) {
+		return;
+	}
+
+	const button = siteNavigation.getElementsByTagName( 'button' )[ 0 ];
+
+	// Return early if the button don't exist.
+	if ( 'undefined' === typeof button ) {
+		return;
+	}
+
+	const menu = siteNavigation.getElementsByTagName( 'ul' )[ 0 ];
+
+	// Hide menu toggle button if menu is empty and return early.
+	if ( 'undefined' === typeof menu ) {
+		button.style.display = 'none';
+		return;
+	}
+
+	if ( ! menu.classList.contains( 'nav-menu' ) ) {
+		menu.classList.add( 'nav-menu' );
+	}
+
+	// Toggle the .toggled class and the aria-expanded value each time the button is clicked.
+	button.addEventListener( 'click', function() {
+		siteNavigation.classList.toggle( 'toggled' );
+
+		if ( button.getAttribute( 'aria-expanded' ) === 'true' ) {
+			button.setAttribute( 'aria-expanded', 'false' );
+		} else {
+			button.setAttribute( 'aria-expanded', 'true' );
+		}
+	} );
+
+	// Remove the .toggled class and set aria-expanded to false when the user clicks outside the navigation.
+	document.addEventListener( 'click', function( event ) {
+		const isClickInside = siteNavigation.contains( event.target );
+
+		if ( ! isClickInside ) {
+			siteNavigation.classList.remove( 'toggled' );
+			button.setAttribute( 'aria-expanded', 'false' );
+		}
+	} );
+
+	// Get all the link elements within the menu.
+	const links = menu.getElementsByTagName( 'a' );
+
+	// Get all the link elements with children within the menu.
+	const linksWithChildren = menu.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+
+	// Toggle focus each time a menu link is focused or blurred.
+	for ( const link of links ) {
+		link.addEventListener( 'focus', toggleFocus, true );
+		link.addEventListener( 'blur', toggleFocus, true );
+	}
+
+	// Toggle focus each time a menu link with children receive a touch event.
+	for ( const link of linksWithChildren ) {
+		link.addEventListener( 'touchstart', toggleFocus, false );
+	}
+
+	/**
+	 * Sets or removes .focus class on an element.
+	 */
+	function toggleFocus() {
+		if ( event.type === 'focus' || event.type === 'blur' ) {
+			let self = this;
+			// Move up through the ancestors of the current link until we hit .nav-menu.
+			while ( ! self.classList.contains( 'nav-menu' ) ) {
+				// On li elements toggle the class .focus.
+				if ( 'li' === self.tagName.toLowerCase() ) {
+					self.classList.toggle( 'focus' );
+				}
+				self = self.parentNode;
+			}
+		}
+
+		if ( event.type === 'touchstart' ) {
+			const menuItem = this.parentNode;
+			event.preventDefault();
+			for ( const link of menuItem.parentNode.children ) {
+				if ( menuItem !== link ) {
+					link.classList.remove( 'focus' );
+				}
+			}
+			menuItem.classList.toggle( 'focus' );
+		}
+	}
+}() );
+
+
+
+$(window).on("scroll", function () {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 80) {
+    $("#site-header").addClass("nav-fixed");
+  } else {
+    $("#site-header").removeClass("nav-fixed");
+  }
+});
+
+//Main navigation Active Class Add Remove
+$(".navbar-toggler").on("click", function () {
+  $("header").toggleClass("active");
+});
+$(document).on("ready", function () {
+  if ($(window).width() > 991) {
+    $("header").removeClass("active");
+  }
+  $(window).on("resize", function () {
+    if ($(window).width() > 991) {
+      $("header").removeClass("active");
+    }
+  });
+});
 
